f9f1841f7172a1f7c0fccba3699b6e17589490bc
Wictor Ducla <wictor.ducla14@gmail.com>
2023-05-16 09:09:24 -0300

    finalizando projeto com a interação do javascript

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const botaoAlterarTema = document.querySelector(".alterar-tema");
+const body = document.querySelector("body");
+//const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroAtivo = body.classList.contains("modo-escuro");
+
+    if (modoEscuroAtivo) {
+        body.classList.remove("modo-escuro");
+
+        //imagemBotaoTrocaDeTema.setAttribute("src", "src/img/sun.png");
+    } else {
+        body.classList.add("modo-escuro");
+
+        //imagemBotaoTrocaDeTema.setAttribute("src", "src/img/moon.png");
+    }
+})
